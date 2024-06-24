0a6ab54d01935ea1649dc87e55e366f5d768449c
tmangaraj <146162112+tmangaraj@users.noreply.github.com>
2024-06-24 09:52:55 +0530

    Create special_number.java

---
+++ Numbers_question/src/special_number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class special_number {
+
+	public static void main(String[] args) {
+		int n,m,sum=0,product=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		n=sc.nextInt();
+		m=n;
+		while(n!=0) {
+			int d=n%10;
+			sum=sum+d;product=product*d;
+			n=n/10;
+		}
+		if(sum+product==m) {
+			System.out.println("special number");
+		}else {
+			System.out.println("not a special number");
+		}
+	}
+
+}
