b0b42107f12ea9e2d2147b513849e5e6656765ee
tmangaraj <146162112+tmangaraj@users.noreply.github.com>
2024-06-24 09:52:33 +0530

    Create natural_number.java

---
+++ Numbers_question/src/natural_number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;                 //print natural number;
+public class natural_number {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=sc.nextInt();
+		for(int i=0;i<=n;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
