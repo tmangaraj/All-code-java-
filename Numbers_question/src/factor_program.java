90d3295a83063b6002f17fb5bdc168aa9f8b0a28
tmangaraj <146162112+tmangaraj@users.noreply.github.com>
2024-06-24 09:52:00 +0530

    Create factor_program.java

---
+++ Numbers_question/src/factor_program.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class factor_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Scanner sc=new Scanner(System.in);
+         System.out.println("enter the number");
+         int n=sc.nextInt();
+         for(int i=1;i<=n;i++) {
+        	 if(n%i==0) {
+        		 System.out.println(i);
+        	 }
+         }
+	}
+
+}
