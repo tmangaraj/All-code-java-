04324c63393c5119e0b3095c2b9faa2fc1564b11
tmangaraj <146162112+tmangaraj@users.noreply.github.com>
2024-06-30 10:20:48 +0530

    Create hollow_decreasing_triangle.java

---
+++ pattern_question/hollow_decreasing_triangle.java
@@ -0,0 +1,20 @@
+package pattern_question;
+import java.util.Scanner;
+public class hollow_decreasing_triangle {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=i;j<=n;j++) {
+				if(i==1||j==n||j==i)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
